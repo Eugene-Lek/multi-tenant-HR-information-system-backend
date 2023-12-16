b01d3c5ac1d61020ad05d5e548177d2cef61b39d
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-12-16 17:06:13 +0800

    update: multiple updates

--- routes/routes.go
+++ routes/routes.go
@@ -4,29 +4,34 @@ import (
 	"net/http"
 	
 	"github.com/gorilla/mux"
+	"github.com/go-playground/validator/v10"
 )
 
 type Storage interface {
-	CreateTenant(name string) error
-	CreateDivision(tenant string, name string) error
-	CreateDepartment(tenant string, division string, name string) error
+	CreateTenant(tenant Tenant) error
+	CreateDivision(division Division) error
+	CreateDepartment(department Department) error
+	CreateUser(user User) error
+	CreateAppointment(appointment Appointment) error
 }
 
 type Router struct {
 	router *mux.Router
 	storage Storage
+	validate *validator.Validate	
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	r.router.ServeHTTP(w, req)
 }
 
-func NewRouter(storage Storage) *Router {
+func NewRouter(storage Storage, validate *validator.Validate) *Router {
 	router := mux.NewRouter()
 
 	r := &Router{
 		router: router,
 		storage: storage,
+		validate: validate,
 	}
 
 	tenantRouter := r.router.PathPrefix("/api/{tenant}").Subrouter()
@@ -34,9 +39,9 @@ func NewRouter(storage Storage) *Router {
 	tenantRouter.HandleFunc("/{division}", r.handleCreateDivision).Methods("POST")
 	tenantRouter.HandleFunc("/{division}/{department}", r.handleCreateDepartment).Methods("POST")
 
-	//userRouter := tenantRouter.PathPrefix("/users").Subrouter()
-	//userRouter.HandleFunc("/{email}", r.handleUser)
-	//userRouter.HandleFunc("", )
+	userRouter := tenantRouter.PathPrefix("/users").Subrouter()
+	userRouter.HandleFunc("/{email}", r.handleCreateUser)
+	userRouter.HandleFunc("/{user-id}/appointments/{id}", r.handleCreateAppointment)
 
 	//jobRequisitionRouter := tenantRouter.PathPrefix("/job-requisition").Subrouter()
 	//jobRequisitionRouter.HandleFunc("", )	
