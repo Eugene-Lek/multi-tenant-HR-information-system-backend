a6d323b8f2586765cb6a9566f9373385691acbf3
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-12-16 17:05:50 +0800

    refactor: types

--- postgres/tenant.go
+++ postgres/tenant.go
@@ -6,18 +6,19 @@ import (
 	"github.com/lib/pq"
 
 	"multi-tenant-HR-information-system-backend/errors"
+	"multi-tenant-HR-information-system-backend/routes"
 )
 
-func (postgres *postgresStorage) CreateTenant(name string) error {
+func (postgres *postgresStorage) CreateTenant(tenant routes.Tenant) error {
 	query := "INSERT INTO tenant (name) VALUES ($1)"
-	_, err := postgres.db.Exec(query, name)
+	_, err := postgres.db.Exec(query, tenant.Name)
 
 	if pgErr, ok := err.(*pq.Error); ok {
 		// 23505 corresponds to the Unique Violation error
 		if pgErr.Code == "23505" {
 			return &errors.ClientError{
 				Code:    "SINGLE-ATTRIBUTE-UNIQUE-VIOLATION",
-				Message: fmt.Sprintf(errors.SingleAttributeUniqueViolation, pgErr.Table, name, name),
+				Message: fmt.Sprintf(errors.SingleAttributeUniqueViolation, pgErr.Table, tenant.Name, tenant.Name),
 			}
 		} else {
 			return &errors.InternalError{
@@ -33,24 +34,24 @@ func (postgres *postgresStorage) CreateTenant(name string) error {
 	return nil
 }
 
-func (postgres *postgresStorage) CreateDivision(tenant string, name string) error {
+func (postgres *postgresStorage) CreateDivision(division routes.Division) error {
 	query := "INSERT INTO division (tenant, name) VALUES ($1, $2)"
-	_, err := postgres.db.Exec(query, tenant, name)
+	_, err := postgres.db.Exec(query, division.Tenant, division.Name)
 	if pgErr, ok := err.(*pq.Error); ok {
 		// 23505 corresponds to the
 		switch pgErr.Code {
 		case "23505":
 			//Unique Violation error
-			existingDivision := fmt.Sprintf(`tenant "%s" & division "%s"`, tenant, name)
+			divisionIdentifiers := fmt.Sprintf(`tenant "%s" & division "%s"`, division.Tenant, division.Name)
 			return &errors.ClientError{
 				Code:    "MULTI-ATTRIBUTE-UNIQUE-VIOLATION",
-				Message: fmt.Sprintf(errors.MultiAttributeUniqueViolation, pgErr.Table, existingDivision),
+				Message: fmt.Sprintf(errors.MultiAttributeUniqueViolation, pgErr.Table, divisionIdentifiers),
 			}
 		case "23503":
 			//Foreign Key Violation error
 			return &errors.ClientError{
 				Code:    "INSERT-FOREIGN-KEY-VIOLATION",
-				Message: fmt.Sprintf(errors.InsertForeignKeyViolation, tenant, "tenant"),
+				Message: fmt.Sprintf(errors.InsertForeignKeyViolation, division.Tenant, "tenant"),
 			}
 		default:
 			return &errors.InternalError{
@@ -66,25 +67,25 @@ func (postgres *postgresStorage) CreateDivision(tenant string, name string) erro
 	return nil
 }
 
-func (postgres *postgresStorage) CreateDepartment(tenant string, division string, name string) error {
+func (postgres *postgresStorage) CreateDepartment(department routes.Department) error {
 	query := "INSERT INTO department (tenant, division, name) VALUES ($1, $2, $3)"
-	_, err := postgres.db.Exec(query, tenant, division, name)
+	_, err := postgres.db.Exec(query, department.Tenant, department.Division, department.Name)
 	if pgErr, ok := err.(*pq.Error); ok {
 		//TODO fill in error handling
 		switch pgErr.Code {
 		//Unique violation
 		case "23505":
-			departmentExists := fmt.Sprintf(`tenant "%s", division "%s", and department "%s"`, tenant, division, name)
+			departmentIdentifiers := fmt.Sprintf(`tenant "%s", division "%s", and department "%s"`, department.Tenant, department.Division, department.Name)
 			return &errors.ClientError{
 				Code: "MULTI-ATTRIBUTE-UNIQUE-VIOLATION",
-				Message: fmt.Sprintf(errors.MultiAttributeUniqueViolation, "department", departmentExists),
+				Message: fmt.Sprintf(errors.MultiAttributeUniqueViolation, "department",departmentIdentifiers),
 			}
 		// Foreign key violation
 		case "23503":
-			invalidTenantDivision := fmt.Sprintf("%s-%s", tenant, division)
+			tenantDivisionCombination := fmt.Sprintf("%s-%s", department.Tenant, department.Division)
 			return &errors.ClientError{
 				Code: "INSERT-FOREIGN-KEY-VIOLATION",
-				Message: fmt.Sprintf(errors.InsertForeignKeyViolation, invalidTenantDivision, "tenant-division combination"),
+				Message: fmt.Sprintf(errors.InsertForeignKeyViolation, tenantDivisionCombination, "tenant-division combination"),
 			}
 		default:
 			return &errors.InternalError{
--- routes/tenant.go
+++ routes/tenant.go
@@ -8,9 +8,32 @@ import  (
 	"github.com/gorilla/mux"
 )
 
+type Tenant struct {
+	Name string `validate:"required"`
+	CreatedAt string
+	UpdatedAt string
+}
+
+type Division struct {
+	Name string
+	Tenant string
+	CreatedAt string
+	UpdatedAt string
+}
+
+type Department struct {
+	Name string
+	Tenant string
+	Division string
+	CreatedAt string
+	UpdatedAt string
+}
+
 func (router *Router) handleCreateTenant(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	tenant := vars["tenant"]
+	tenant := Tenant{
+		Name: vars["tenant"],
+	}
 	//TODO input validation
 
 	err := router.storage.CreateTenant(tenant)
@@ -23,12 +46,14 @@ func (router *Router) handleCreateTenant(w http.ResponseWriter, r *http.Request)
 
 func (router *Router) handleCreateDivision(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	tenant := vars["tenant"]	
-	division := vars["division"]	
+	division := Division{
+		Name: vars["division"],
+		Tenant: vars["tenant"],
+	}
 
 	//TODO parameter validation
 
-	err := router.storage.CreateDivision(tenant, division)
+	err := router.storage.CreateDivision(division)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
@@ -38,11 +63,13 @@ func (router *Router) handleCreateDivision(w http.ResponseWriter, r *http.Reques
 
 func (router *Router) handleCreateDepartment (w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	tenant := vars["tenant"]
-	division := vars["division"]
-	department := vars["department"]
+	department := Department{
+		Name: vars["department"],
+		Tenant: vars["tenant"],
+		Division: vars["division"],
+	}
 
-	err := router.storage.CreateDepartment(tenant, division, department)
+	err := router.storage.CreateDepartment(department)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
