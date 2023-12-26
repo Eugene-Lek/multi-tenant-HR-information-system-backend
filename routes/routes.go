275d5db06c92ae1a2b340b7772142300c69e92f8
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-12-26 23:45:24 +0800

    update: route paths & error handling

--- routes/routes.go
+++ routes/routes.go
@@ -1,25 +1,19 @@
 package routes
 
 import (
-	"context"
 	"net/http"
-	"strings"
-	"encoding/json"
-	"reflect"
 
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
-
-	"multi-tenant-HR-information-system-backend/errors"	
 )
 
 type Storage interface {
-	CreateTenant(tenant Tenant) errors.HttpError
-	CreateDivision(division Division) errors.HttpError
-	CreateDepartment(department Department) errors.HttpError
-	CreateUser(user User) errors.HttpError
-	CreateAppointment(appointment Appointment) errors.HttpError
+	CreateTenant(tenant Tenant) error
+	CreateDivision(division Division) error
+	CreateDepartment(department Department) error
+	CreateUser(user User) error
+	CreateAppointment(appointment Appointment) error
 }
 
 type Router struct {
@@ -46,14 +40,16 @@ func NewRouter(storage Storage, universalTranslator *ut.UniversalTranslator, val
 	r.router.Use(errorHandlingMiddleware)
 	r.router.Use(getLanguageMiddleware)
 
-	tenantRouter := r.router.PathPrefix("/api/{tenant}").Subrouter()
+	apiRouter := r.router.PathPrefix("/api").Subrouter()
+
+	tenantRouter := apiRouter.PathPrefix("/tenants/{tenant}").Subrouter()
 	tenantRouter.HandleFunc("", r.handleCreateTenant).Methods("POST")
-	tenantRouter.HandleFunc("/{division}", r.handleCreateDivision).Methods("POST")
-	tenantRouter.HandleFunc("/{division}/{department}", r.handleCreateDepartment).Methods("POST")
+	tenantRouter.HandleFunc("/divisions/{division}", r.handleCreateDivision).Methods("POST")
+	tenantRouter.HandleFunc("/divisions/{division}/departments/{department}", r.handleCreateDepartment).Methods("POST")
 
 	userRouter := tenantRouter.PathPrefix("/users").Subrouter()
-	userRouter.HandleFunc("/{email}", r.handleCreateUser)
-	userRouter.HandleFunc("/{user-id}/appointments/{id}", r.handleCreateAppointment)
+	userRouter.HandleFunc("/{user-id}", r.handleCreateUser).Methods("POST")
+	userRouter.HandleFunc("/{user-id}/appointments/{id}", r.handleCreateAppointment).Methods("POST")
 
 	//jobRequisitionRouter := tenantRouter.PathPrefix("/job-requisition").Subrouter()
 	//jobRequisitionRouter.HandleFunc("", )	
@@ -61,92 +57,14 @@ func NewRouter(storage Storage, universalTranslator *ut.UniversalTranslator, val
 	return r
 }
 
-func validateStruct(validate *validator.Validate, translator ut.Translator, s interface{}) errors.HttpError {
+// Validates a struct instance, translates the errors to error messages and returns an error that collates all the error messages
+func validateStruct(validate *validator.Validate, translator ut.Translator, s interface{}) error {
 	err := validate.Struct(s)
 	if err != nil {
 		validationErrors := err.(validator.ValidationErrors)
 		errorMessages := validationErrors.Translate(translator)
-		return errors.NewInputValidationError(errorMessages)		
+		return NewInputValidationError(errorMessages)		
 	}
 
 	return nil
-}
-
-type contextKey int
-
-const (
-	errorHandlingKey contextKey = iota	
-	languageKey
-)
-
-func getLanguageMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		acceptLanguageHeader := r.Header.Get("Accept-Language")
-		languageValue := strings.Split(acceptLanguageHeader, "-")[0]
-
-		r = r.WithContext(context.WithValue(r.Context(), languageKey, languageValue))
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-// Fetches the locale from the Request Context & uses that to fetch the desired translator
-func getAppropriateTranslator(r *http.Request, universalTranslator *ut.UniversalTranslator) (ut.Translator, errors.HttpError) {
-	language, ok := r.Context().Value(languageKey).(string)
-	if !ok {
-		return nil, errors.NewInternalServerError("Could not obtain preferred language")
-	}
-	translator, _ := universalTranslator.GetTranslator(language)	
-
-	return translator, nil
-}
-
-type ErrorWrapper struct {
-	Error errors.HttpError
-}
-
-func errorHandlingMiddleware(next http.Handler) http.Handler {
-	type errorResponseBody struct {
-		Code string `json:"code"`
-		Message string `json:"message"`
-	}
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Create a pointer (address) to the ErrorWrapper & store it in the request context
-		// In the route handlers, the ErrorWrapper & hence its error object value will be assigned to this pointer
-		// This way, the middleware can access the error object via the ErrorWrapper
-		// An ErrorWrapper is necessary because the pointer created must point to a struct, not an interface. 
-		// Otherwise, you cannot assign the error object to the pointer as it does not implement the pointer to the interface
-
-		errWrapper := new(ErrorWrapper)
-		r = r.WithContext(context.WithValue(r.Context(), errorHandlingKey, errWrapper))
-
-		next.ServeHTTP(w, r)
-
-		errWrapper, ok := r.Context().Value(errorHandlingKey).(*ErrorWrapper)
-		if !ok {
-			w.WriteHeader(http.StatusInternalServerError)
-			//w.Write()
-			return
-		}
-
-		httpErr := errWrapper.Error
-
-		body := errorResponseBody{
-			Code: reflect.TypeOf(httpErr).Name(), //TODO derive error code from error type name
-			Message: httpErr.Error(),
-		}
-
-		w.Header().Add("content-type", "application/json")
-		w.WriteHeader(httpErr.Status())
-		json.NewEncoder(w).Encode(body)
-	})
-}
-
-// Sends the HttpError to the error handling middleware via the Request Context
-// The error is assigned to an existing pointer in the Request context
-func sendToErrorHandlingMiddleware(err errors.HttpError, r *http.Request) {
-	if errWrapper, ok := r.Context().Value(errorHandlingKey).(*ErrorWrapper); ok {
-		errWrapper.Error = err
-	}		
 }
