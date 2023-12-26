bb93db1a8a4973ca07bfe64c41c404e43d53a732
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-12-26 23:46:27 +0800

    add: password, totp, input validations

--- routes/user.go
+++ routes/user.go
@@ -3,18 +3,23 @@ package routes
 import (
 	"encoding/json"
 	"net/http"
-	"log"
+	"crypto/rand"
+	mathrand "math/rand"
+	"strings"
+	"math/big"
 
 	"github.com/gorilla/mux"
+	"github.com/pquerna/otp/totp"
+	"github.com/alexedwards/argon2id"
 )
 
 
 type User struct {
-	Id string
-	Email string
-	Tenant string
-	Division string
-	Department string
+	Id string `validate:"required,notBlank,uuid" name:"user id"`
+	Email string `validate:"required,notBlank,email" name:"user email"`
+	Tenant string `validate:"required,notBlank" name:"tenant name"`
+	Division string `validate:"required,notBlank" name:"division name"`
+	Department string `validate:"required,notBlank" name:"department name"`
 	Password string
 	TotpSecretKey string
 	CreatedAt string
@@ -23,66 +28,158 @@ type User struct {
 }
 
 type Appointment struct {
-	Title string
-	Tenant string
-	Division string
-	Department string
-	UserId string
-	StartDate string
-	EndDate string
+	Title string `validate:"required,notBlank" name:"appointment title"`
+	Tenant string `validate:"required,notBlank" name:"tenant name"`
+	Division string `validate:"required,notBlank" name:"division name"`
+	Department string `validate:"required,notBlank" name:"department name"`
+	UserId string `validate:"required,notBlank" name:"user id"`
+	StartDate string `validate:"required,notBlank,isIsoDate" name:"start date"`
+	EndDate string `validate:"omitempty,isIsoDate" name:"end date"`
 	CreatedAt string
 	UpdatedAt string
 }
 
+func generateRandomPassword(length int, minLower int, minUpper int, minNumber int, minSpecial int) string {
+	lowerCharSet   := "abcdedfghijklmnopqrst"
+	upperCharSet   := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	specialCharSet := "!@#$%&*"
+	numberSet      := "0123456789"
+	allCharSet     := lowerCharSet + upperCharSet + specialCharSet + numberSet	
+
+	var password strings.Builder
+	for i:=0; i < minLower; i++ {
+		random, err := rand.Int(rand.Reader, big.NewInt(int64(len(lowerCharSet))))
+		if err != nil {
+			panic(err)
+		}
+		password.WriteString(string(lowerCharSet[random.Int64()]))
+	}
+
+	for i:=0; i < minUpper; i++ {
+		random, err := rand.Int(rand.Reader, big.NewInt(int64(len(upperCharSet))))
+		if err != nil {
+			panic(err)
+		}
+		password.WriteString(string(upperCharSet[random.Int64()]))
+	}
+
+	for i:=0; i < minSpecial; i++ {
+		random, err := rand.Int(rand.Reader, big.NewInt(int64(len(specialCharSet))))
+		if err != nil {
+			panic(err)
+		}
+		password.WriteString(string(specialCharSet[random.Int64()]))
+	}
+
+	for i:=0; i < minNumber; i++ {
+		random, err := rand.Int(rand.Reader, big.NewInt(int64(len(numberSet))))
+		if err != nil {
+			panic(err)
+		}
+		password.WriteString(string(numberSet[random.Int64()]))
+	}
+
+	remainingChars := length - minLower - minUpper - minSpecial - minNumber
+	if remainingChars > 0 {
+		for i:=0; i < remainingChars; i++ {
+			random, err := rand.Int(rand.Reader, big.NewInt(int64(len(allCharSet))))
+			if err != nil {
+				panic(err)
+			}
+			password.WriteString(string(allCharSet[random.Int64()]))			
+		}
+	}
+
+	runeString := []rune(password.String()) // Convert to rune so that the string can be manipulated (strings are read-only)
+	mathrand.Shuffle(len(runeString), func(i int, j int) {
+		runeString[i], runeString[j] = runeString[j], runeString[i]
+	})
+
+	return string(runeString)
+}
+
 func (router *Router) handleCreateUser(w http.ResponseWriter, r *http.Request) {
 	type requestBody struct {
+		Email string
 		Division string 
 		Department string
 	}
 
+	type responseBody struct {
+		Password string `json:"password"`
+		TotpSecretKey string `json:"totpSecretKey"`
+	}
+
+	// Parse inputs
 	var body requestBody
 	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
-		log.Fatal(err.Error())
+		err := NewInvalidJSONError()
+		sendToErrorHandlingMiddleware(err, r)
+		return
 	}
 
 	vars := mux.Vars(r)
 
 	// Create default password + totpsecretkey
+	defaultPassword := generateRandomPassword(12, 2, 2, 2, 2)
+	hashedPassword, err := argon2id.CreateHash(defaultPassword, argon2id.DefaultParams)
+	if err != nil {
+		err := NewInternalServerError(err)
+		sendToErrorHandlingMiddleware(err, r)
+		return
+	}	
+
+	key, err := totp.Generate(totp.GenerateOpts{
+		Issuer:      "HRIS.com",
+		AccountName: body.Email,
+		SecretSize: 20,
+		Period: 30,
+	})
+	if err != nil {
+		err := NewInternalServerError(err)
+		sendToErrorHandlingMiddleware(err, r)
+		return
+	}
 
 	user := User{
-		Email: vars["email"],
+		Id: vars["user-id"],
+		Email: body.Email,
 		Tenant: vars["tenant"],
 		Division: body.Division,	
 		Department: body.Department,
+		Password: hashedPassword,
+		TotpSecretKey: key.Secret(),
 	}
 
-	//TODO: Input validation
-	translator, httpErr := getAppropriateTranslator(r, router.universalTranslator)
-	if httpErr != nil {
-		sendToErrorHandlingMiddleware(httpErr, r)
-		log.Print(httpErr.Error())
-
+	//Input validation
+	translator, err := getAppropriateTranslator(r, router.universalTranslator)
+	if err != nil {
+		sendToErrorHandlingMiddleware(err, r)
 		return
 	}	
 	
-	httpErr = validateStruct(router.validate, translator, user)
-	if httpErr != nil {
-		sendToErrorHandlingMiddleware(httpErr, r)
-		log.Print(httpErr.Error())
-
+	err = validateStruct(router.validate, translator, user)
+	if err != nil {
+		sendToErrorHandlingMiddleware(err, r)
 		return
 	}	
 
-	httpErr = router.storage.CreateUser(user)
-	if httpErr != nil {
-		sendToErrorHandlingMiddleware(httpErr, r)
-		// Logging
-
+	// Make DB query
+	err = router.storage.CreateUser(user)
+	if err != nil {
+		sendToErrorHandlingMiddleware(err, r)
 		return	
 	}
 
+	resBody := responseBody{
+		Password: defaultPassword,
+		TotpSecretKey: key.Secret(),
+	}
+
+	w.Header().Add("content-type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(resBody)
 }
 
 
@@ -99,7 +196,9 @@ func (router *Router) handleCreateAppointment(w http.ResponseWriter, r *http.Req
 
 	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
-		log.Fatal(err.Error())
+		err := NewInvalidJSONError()
+		sendToErrorHandlingMiddleware(err, r)
+		return
 	}
 
 	vars := mux.Vars(r)
@@ -115,27 +214,21 @@ func (router *Router) handleCreateAppointment(w http.ResponseWriter, r *http.Req
 	}	
 
 	//TODO Input validation
-	translator, httpErr := getAppropriateTranslator(r, router.universalTranslator)
-	if httpErr != nil {
-		sendToErrorHandlingMiddleware(httpErr, r)
-		log.Print(httpErr.Error())
-
+	translator, err := getAppropriateTranslator(r, router.universalTranslator)
+	if err != nil {
+		sendToErrorHandlingMiddleware(err, r)
 		return
 	}	
 	
-	httpErr = validateStruct(router.validate, translator, userAppointment)
-	if httpErr != nil {
-		sendToErrorHandlingMiddleware(httpErr, r)
-		log.Print(httpErr.Error())
-
+	err = validateStruct(router.validate, translator, userAppointment)
+	if err != nil {
+		sendToErrorHandlingMiddleware(err, r)
 		return
 	}		
 
-	httpErr = router.storage.CreateAppointment(userAppointment)
-	if httpErr != nil {
-		sendToErrorHandlingMiddleware(httpErr, r)
-		// Logging
-
+	err = router.storage.CreateAppointment(userAppointment)
+	if err != nil {
+		sendToErrorHandlingMiddleware(err, r)
 		return
 	}
 
