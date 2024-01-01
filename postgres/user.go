e2dfbd3301ba6afa6fbbf276dbd521aeff6e7430
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2024-01-01 21:58:36 +0800

    add: getUsers model

--- postgres/user.go
+++ postgres/user.go
@@ -1,6 +1,7 @@
 package postgres
 
 import (
+	"database/sql"
 	"fmt"
 
 	"github.com/lib/pq"
@@ -10,9 +11,9 @@ import (
 
 func (postgres *postgresStorage) CreateUser(user routes.User) error {
 	query := `
-		INSERT INTO user_account (id, email, tenant, division, department, password, totp_secret_key) 
-		VALUES ($1, $2, $3, $4, $5, $6, $7)`
-	_, err := postgres.db.Exec(query, user.Id, user.Email, user.Tenant, user.Division, user.Department, user.Password, user.TotpSecretKey)
+		INSERT INTO user_account (id, email, tenant, password, totp_secret_key) 
+		VALUES ($1, $2, $3, $4, $5)`
+	_, err := postgres.db.Exec(query, user.Id, user.Email, user.Tenant, user.Password, user.TotpSecretKey)
 	if pgErr, ok := err.(*pq.Error); ok {
 		switch pgErr.Code {
 		case "23505":
@@ -31,6 +32,52 @@ func (postgres *postgresStorage) CreateUser(user routes.User) error {
 
 	return nil
 
+}
+
+func (postgres postgresStorage) GetUsers(userFilter routes.User) ([]routes.User, error) {
+	conditions := []string{}
+	values := []any{}
+
+	if userFilter.Id != "" {
+		conditions = append(conditions, "id")
+		values = append(values, userFilter.Id)
+	}
+
+	if userFilter.Email != "" {
+		conditions = append(conditions, "email")
+		values = append(values, userFilter.Email)
+	}
+
+	if userFilter.Tenant != "" {
+		conditions = append(conditions, "tenant")
+		values = append(values, userFilter.Tenant)
+	}
+
+	query := newDynamicConditionQuery("SELECT * FROM user_account", conditions)
+
+	rows, err := postgres.db.Query(query, values...)
+	if err != nil {
+		return nil, routes.NewInternalServerError(err)
+	}
+	defer rows.Close()
+
+	var fetchedUsers []routes.User
+
+	for rows.Next() {
+		var user routes.User
+		var lastLogin sql.NullString // last_login may be null
+
+		if err := rows.Scan(&user.Id, &user.Email, &user.Tenant, &user.Password,
+			&user.TotpSecretKey, &user.CreatedAt, &user.UpdatedAt, &lastLogin); err != nil {
+			return nil, routes.NewInternalServerError(err)
+		}
+
+		user.LastLogin = lastLogin.String
+
+		fetchedUsers = append(fetchedUsers, user)
+	}
+
+	return fetchedUsers, nil
 }
 
 func (postgres *postgresStorage) CreateAppointment(appointment routes.Appointment) error {
