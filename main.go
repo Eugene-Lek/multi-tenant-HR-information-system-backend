c54469575c2a8cf9143c7d3fceeb2d555cb06184
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2024-01-04 15:41:46 +0800

    add: set up logging

--- main.go
+++ main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/quasoft/memstore"
+	pgadapter "github.com/casbin/casbin-pg-adapter"
 	"github.com/casbin/casbin/v2"
-	"github.com/casbin/casbin/v2/util"	
-	pgadapter "github.com/casbin/casbin-pg-adapter"	
+	"github.com/casbin/casbin/v2/util"
 	"github.com/go-pg/pg/v10"
+	"github.com/quasoft/memstore"
 
 	"multi-tenant-HR-information-system-backend/postgres"
 	"multi-tenant-HR-information-system-backend/routes"
 )
 
 func main() {
+	// Logger is instantiated first so it can be used to log the application start up
+	logOutputMedium := os.Stdout
+	rootLogger := routes.NewRootLogger(logOutputMedium)
 
 	listenAddress := "localhost:3000"
 	connString := "host=localhost port=5433 user=hr_information_system password=abcd1234 dbname=hr_information_system sslmode=disable"
 
 	storage, err := postgres.NewPostgresStorage(connString)
 	if err != nil {
-		log.Fatalf("Could not connect to database: %s", err)
+		rootLogger.Fatal("DB-CONNECTION-FAILED", "errorMessage", fmt.Sprintf("Could not connect to database: %s", err))
+	} else {
+		opts, _ := pg.ParseURL("postgres://hr_information_system:abcd1234@localhost:5433/hr_information_system?sslmode=disable")
+		rootLogger.Info("DB-CONNECTION-ESTABLISHED", "user", opts.User, "host", opts.Addr, "database", opts.Database)
 	}
 
 	// A Translator maps tags to text templates (you must register these tags & templates yourself)
@@ -34,44 +40,46 @@ func main() {
 
 	validate, err := routes.NewValidator(universalTranslator)
 	if err != nil {
-		log.Fatalf("Could not instantiate the validator: %s", err)
+		rootLogger.Fatal("VALIDATOR-INSTANTIATION-FAILED", "errorMessage", fmt.Sprintf("Could not instantiate validator: %s", err))
+	} else {
+		rootLogger.Info("VALIDATOR-INSTANTIATED")
 	}
-
-	logOutputMedium := os.Stdout
-	rootLogger := routes.NewRootLogger(logOutputMedium)
 
 	// TODO: create env file to set authentication (hashing/signing) & encryption keys
 	sessionStore := memstore.NewMemStore(
 		[]byte("authkey123"),
 		[]byte("enckey12341234567890123456789012"),
 	)
+	rootLogger.Info("SESSION-STORE-CONNECTION-ESTABLISHED")
 
-	opts := &pg.Options{
-		Addr:     ":5433",
-		User:     "hr_information_system",
-		Password: "abcd1234",
-		Database: "hr_information_system",
-	}
-
+	opts, _ := pg.ParseURL("postgres://hr_information_system:abcd1234@localhost:5433/hr_information_system?sslmode=disable")
 	db := pg.Connect(opts)
 	defer db.Close()
 
 	a, err := pgadapter.NewAdapterByDB(db, pgadapter.SkipTableCreate())
 	if err != nil {
-		log.Fatalf("Could not instantiate Authorization Adapter: %s", err)
+		rootLogger.Fatal("AUTHORIZATION-ADAPTER-INSTANTIATION-FAILED", "errorMessage", fmt.Sprintf("Could not instantiate Authorization Adapter: %s", err))
+	} else {
+		rootLogger.Info("AUTHORIZATION-ADAPTER-INSTANTIATED", "user", opts.User, "host", opts.Addr, "database", opts.Database)
 	}
+
 	authEnforcer, err := casbin.NewEnforcer("auth_model.conf", a)
 	if err != nil {
-		log.Fatalf("Could not instantiate Authorization Enforcer: %s", err)
-	}	
+		rootLogger.Fatal("AUTHORIZATION-ENFORCER-INSTANTIATION-FAILED", "errorMessage", fmt.Sprintf("Could not instantiate Authorization Enforcer: %s", err))
+	} else {
+		rootLogger.Info("AUTHORIZATION-ENFORCER-INSTANTIATED")
+	}
+
 	if err := authEnforcer.LoadPolicy(); err != nil {
-		log.Fatalf("Could not load policy into Authorization Enforcer: %s", err)
+		rootLogger.Fatal("AUTHORIZATION-POLICY-LOAD-FAILED", "errorMessage", fmt.Sprintf("Could not load policy into Authorization Enforcer: %s", err))
+	} else {
+		rootLogger.Info("AUTHORIZATION-POLICY-LOADED")
 	}
 	authEnforcer.AddNamedMatchingFunc("g", "KeyMatch2", util.KeyMatch2)
 	authEnforcer.AddNamedDomainMatchingFunc("g", "KeyMatch2", util.KeyMatch2)
 
 	router := routes.NewRouter(storage, universalTranslator, validate, rootLogger, sessionStore, authEnforcer)
 
-	log.Println("API server running on port: ", listenAddress)
 	http.ListenAndServe(listenAddress, router)
+	rootLogger.Info("SERVER-STARTED", "address", listenAddress)
 }
