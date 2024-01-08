7141afc34212fb5ae24e05fb97015857f79f489b
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2024-01-08 22:32:08 +0800

    add: new tests

--- postgres/tenant_test.go
+++ postgres/tenant_test.go
@@ -5,7 +5,7 @@ import (
 	"multi-tenant-HR-information-system-backend/routes"
 )
 
-func (s *PostgresTestSuite) TestCreateTenant() {
+func (s *IntegrationTestSuite) TestCreateTenant() {
 	wantTenant := routes.Tenant{
 		Name: "Macdonalds",
 	}
@@ -15,37 +15,32 @@ func (s *PostgresTestSuite) TestCreateTenant() {
 
 	s.expectSelectQueryToReturnOneRow(
 		"tenant",
-		[]string{"name"},
-		[]string{wantTenant.Name},
+		map[string]string{
+			"name": wantTenant.Name,
+		},
 	)
 }
 
-func (s *PostgresTestSuite) TestCreateTenantAlreadyExists() {
+func (s *IntegrationTestSuite) TestCreateTenantViolatesUniqueConstraint() {
 	wantTenant := routes.Tenant{
 		Name: s.defaultUser.Tenant,
 	}
 
 	err := s.postgres.CreateTenant(wantTenant)
-	s.Equal(false, err == nil, "Error should not be nil")
-
-	httpErr, ok := err.(*routes.HttpError)
-	s.Equal(true, ok, "Error should be HttpError")
-
-	if ok {
-		s.Equal("UNIQUE-VIOLATION-ERROR", httpErr.Code)
-	}
+	s.expectErrorCode(err, "UNIQUE-VIOLATION-ERROR")
 
 	s.expectSelectQueryToReturnOneRow(
 		"tenant",
-		[]string{"name"},
-		[]string{wantTenant.Name},
+		map[string]string{
+			"name": wantTenant.Name,
+		},
 	)
 }
 
-func (s *PostgresTestSuite) TestCreateDivision() {
+func (s *IntegrationTestSuite) TestCreateDivision() {
 	wantDivision := routes.Division{
-		Name:   "Marketing",
 		Tenant: s.defaultUser.Tenant,
+		Name:   "Marketing",		
 	}
 
 	err := s.postgres.CreateDivision(wantDivision)
@@ -53,141 +48,236 @@ func (s *PostgresTestSuite) TestCreateDivision() {
 
 	s.expectSelectQueryToReturnOneRow(
 		"division",
-		[]string{"name", "tenant"},
-		[]string{wantDivision.Name, wantDivision.Tenant},
+		map[string]string{
+			"tenant": wantDivision.Tenant,
+			"name":   wantDivision.Name,
+		},
 	)
 }
 
-func (s *PostgresTestSuite) TestCreateDivisionAlreadyExists() {
+func (s *IntegrationTestSuite) TestCreateDivisionViolatesUniqueConstraint() {
 	wantDivision := routes.Division{
-		Name:   "Marketing",
 		Tenant: s.defaultUser.Tenant,
+		Name:   s.defaultAppointment.Division,		
 	}
 
-	query := "INSERT INTO division (name, tenant) VALUES ($1, $2)"
-	_, err := s.dbRootConn.Exec(query, wantDivision.Name, wantDivision.Tenant)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = s.postgres.CreateDivision(wantDivision)
-	s.Equal(false, err == nil, "Error should not be nil")
-
-	httpErr, ok := err.(*routes.HttpError)
-	s.Equal(true, ok, "Error should be HttpError")
-
-	if ok {
-		s.Equal("UNIQUE-VIOLATION-ERROR", httpErr.Code)
-	}
+	err := s.postgres.CreateDivision(wantDivision)
+	s.expectErrorCode(err, "UNIQUE-VIOLATION-ERROR")
 
 	s.expectSelectQueryToReturnOneRow(
 		"division",
-		[]string{"name", "tenant"},
-		[]string{wantDivision.Name, wantDivision.Tenant},
+		map[string]string{
+			"tenant": wantDivision.Tenant,
+			"name":   wantDivision.Name,
+		},
 	)
 }
 
-func (s *PostgresTestSuite) TestCreateDivisionInvalidTenant() {
+func (s *IntegrationTestSuite) TestCreateDivisionDoesNotViolateUniqueConstraint() {
+	// Seed a tenant and division
+	_, err := s.dbRootConn.Exec("INSERT INTO tenant (name) VALUES ($1)", "Macdonalds")
+	if err != nil {
+		log.Fatalf("Could not seed tenant: %s", err)
+	}
+
+	tests := []struct {
+		name  string
+		input routes.Division
+	}{
+		{
+			"Should not violate unique constraint because tenant is different",
+			routes.Division{
+				Tenant: "Macdonalds",
+				Name:   s.defaultAppointment.Division,
+			},
+		},
+		{
+			"Should not violate unique constraint because name is different",
+			routes.Division{
+				Tenant: "HRIS Enterprises",
+				Name:   "Division2",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.name, func() {
+			err := s.postgres.CreateDivision(test.input)
+			s.Equal(nil, err)
+
+			s.expectSelectQueryToReturnOneRow(
+				"division",
+				map[string]string{
+					"tenant": test.input.Tenant,
+					"name":   test.input.Name,
+				},
+			)
+
+			query := "DELETE FROM division WHERE tenant = $1 AND name = $2"
+			s.dbRootConn.Exec(query, test.input.Tenant, test.input.Name)
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestCreateDivisionViolatesForeignKeyConstraint() {
 	wantDivision := routes.Division{
 		Name:   "Marketing",
 		Tenant: "non-existent",
 	}
 
 	err := s.postgres.CreateDivision(wantDivision)
-	s.Equal(false, err == nil, "Error should not be nil")
-
-	httpErr, ok := err.(*routes.HttpError)
-	s.Equal(true, ok, "Error should be HttpError")
-
-	if ok {
-		s.Equal("INVALID-FOREIGN-KEY-ERROR", httpErr.Code)
-	}
+	s.expectErrorCode(err, "INVALID-FOREIGN-KEY-ERROR")
 
 	s.expectSelectQueryToReturnNoRows(
 		"division",
-		[]string{"name", "tenant"},
-		[]string{wantDivision.Name, wantDivision.Tenant},
+		map[string]string{
+			"tenant": wantDivision.Tenant,
+			"name":   wantDivision.Name,
+		},
 	)
 }
 
-func (s *PostgresTestSuite) TestCreateDepartment() {
+func (s *IntegrationTestSuite) TestCreateDepartment() {
 	wantDepartment := routes.Department{
-		Name:     "Outreach",
 		Tenant:   s.defaultUser.Tenant,
-		Division: "Marketing",
+		Division: s.defaultAppointment.Division,
+		Name:     "Outreach",
 	}
 
-	query := "INSERT INTO division (name, tenant) VALUES ($1, $2)"
-	_, err := s.dbRootConn.Exec(query, wantDepartment.Division, wantDepartment.Tenant)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = s.postgres.CreateDepartment(wantDepartment)
+	err := s.postgres.CreateDepartment(wantDepartment)
 	s.Equal(nil, err, "Should not return an error")
 
 	s.expectSelectQueryToReturnOneRow(
 		"department",
-		[]string{"name", "tenant", "division"},
-		[]string{wantDepartment.Name, wantDepartment.Tenant, wantDepartment.Division},
+		map[string]string{
+			"tenant":   wantDepartment.Tenant,
+			"division": wantDepartment.Division,
+			"name":     wantDepartment.Name,
+		},
 	)
 }
 
-func (s *PostgresTestSuite) TestCreateDepartmentAlreadyExists() {
+func (s *IntegrationTestSuite) TestCreateDepartmentViolatesUniqueConstraint() {
 	wantDepartment := routes.Department{
-		Name:     "Outreach",
 		Tenant:   s.defaultUser.Tenant,
-		Division: "Marketing",
-	}
-
-	query := "INSERT INTO division (name, tenant) VALUES ($1, $2)"
-	_, err := s.dbRootConn.Exec(query, wantDepartment.Division, wantDepartment.Tenant)
-	if err != nil {
-		log.Fatal(err)
-	}
-	query = "INSERT INTO department (name, tenant, division) VALUES ($1, $2, $3)"
-	_, err = s.dbRootConn.Exec(query, wantDepartment.Name, wantDepartment.Tenant, wantDepartment.Division)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = s.postgres.CreateDepartment(wantDepartment)
-	s.Equal(false, err == nil, "Error should not be nil")
-
-	httpErr, ok := err.(*routes.HttpError)
-	s.Equal(true, ok, "Error should be HttpError")
-
-	if ok {
-		s.Equal("UNIQUE-VIOLATION-ERROR", httpErr.Code)
-	}
-
-	s.expectSelectQueryToReturnOneRow(
-		"department",
-		[]string{"name", "tenant", "division"},
-		[]string{wantDepartment.Name, wantDepartment.Tenant, wantDepartment.Division},
-	)
-}
-
-func (s *PostgresTestSuite) TestCreateDepartmentInvalidDivision() {
-	wantDepartment := routes.Department{
-		Name:     "Outreach",
-		Tenant:   s.defaultUser.Tenant,
-		Division: "Marketing",
+		Division: s.defaultAppointment.Division,
+		Name:     s.defaultAppointment.Department,
 	}
 
 	err := s.postgres.CreateDepartment(wantDepartment)
-	s.Equal(false, err == nil, "Error should not be nil")
+	s.expectErrorCode(err, "UNIQUE-VIOLATION-ERROR")
 
-	httpErr, ok := err.(*routes.HttpError)
-	s.Equal(true, ok, "Error should be HttpError")
+	s.expectSelectQueryToReturnOneRow(
+		"department",
+		map[string]string{
+			"tenant":   wantDepartment.Tenant,
+			"division": wantDepartment.Division,
+			"name":     wantDepartment.Name,
+		},
+	)
+}
 
-	if ok {
-		s.Equal("INVALID-FOREIGN-KEY-ERROR", httpErr.Code)
+func (s *IntegrationTestSuite) TestCreateDepartmentDoesNotViolateUniqueConstraint() {
+	// Seed a tenant and a division
+	_, err := s.dbRootConn.Exec("INSERT INTO tenant (name) VALUES ($1)", "Macdonalds")
+	if err != nil {
+		log.Fatalf("Could not seed tenant: %s", err)
 	}
 
-	s.expectSelectQueryToReturnNoRows(
-		"department",
-		[]string{"name", "tenant", "division"},
-		[]string{wantDepartment.Name, wantDepartment.Tenant, wantDepartment.Division},
-	)
+	_, err = s.dbRootConn.Exec("INSERT INTO division (tenant, name) VALUES ($1, $2), ($3, $4)", 
+								"Macdonalds", s.defaultAppointment.Division,
+								"HRIS Enterprises", "Marketing",								
+							)
+	if err != nil {
+		log.Fatalf("Could not seed tenant: %s", err)
+	}
+
+	tests := []struct {
+		name  string
+		input routes.Department
+	}{
+		{
+			"Should not violate unique constraint because tenant is different",
+			routes.Department{
+				Tenant:   "Macdonalds",
+				Division: s.defaultAppointment.Division,
+				Name:     s.defaultAppointment.Department,
+			},
+		},
+		{
+			"Should not violate unique constraint because division is different",
+			routes.Department{
+				Tenant:   s.defaultAppointment.Tenant,
+				Division: "Marketing",
+				Name:     s.defaultAppointment.Department,
+			},
+		},
+		{
+			"Should not violate unique constraint because name is different",
+			routes.Department{
+				Tenant:   s.defaultAppointment.Tenant,
+				Division: s.defaultAppointment.Division,
+				Name:     "Customer Support",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.name, func() {
+			err := s.postgres.CreateDepartment(test.input)
+			s.Equal(nil, err)
+
+			s.expectSelectQueryToReturnOneRow(
+				"department",
+				map[string]string{
+					"tenant":   test.input.Tenant,
+					"division": test.input.Division,
+					"name":     test.input.Name,
+				},
+			)
+
+			query := "DELETE FROM department WHERE tenant = $1 AND division = $2 AND name = $3"
+			s.dbRootConn.Exec(query, test.input.Tenant, test.input.Division, test.input.Name)
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestCreateDepartmentViolatesForeignKeyConstraint() {
+	tests := []struct {
+		name  string
+		input routes.Department
+	}{
+		{
+			"Should violate foreign key constraint as division doesn't exist",
+			routes.Department{
+				Name:     "Outreach",
+				Tenant:   s.defaultUser.Tenant,
+				Division: "Marketing",
+			},
+		},
+		{
+			"Should violate foreign key constraint as tenant doesn't exist",
+			routes.Department{
+				Name:     "Outreach",
+				Tenant:   "Does not exist",
+				Division: "Anything",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.name, func() {
+			err := s.postgres.CreateDepartment(test.input)
+			s.expectErrorCode(err, "INVALID-FOREIGN-KEY-ERROR")
+
+			s.expectSelectQueryToReturnNoRows(
+				"department",
+				map[string]string{
+					"tenant":   test.input.Tenant,
+					"division": test.input.Division,
+					"name":     test.input.Name,
+				},
+			)
+		})
+	}
 }
--- postgres/user_test.go
+++ postgres/user_test.go
@@ -1,7 +1,397 @@
 package postgres
 
-import ()
+import (
+	"fmt"
+	"log"
 
-func (s *PostgresTestSuite) TestCreateUser() {
+	"multi-tenant-HR-information-system-backend/routes"
+)
 
+type userTestCase struct {
+	name  string
+	input routes.User
+}
+
+func (s *IntegrationTestSuite) TestCreateUser() {
+	wantUser := routes.User{
+		Id:     "d6cfdb0d-b9ae-4f17-ba0a-b141391c42af",
+		Email:  "test@gmail.com",
+		Tenant: s.defaultUser.Tenant,
+	}
+
+	err := s.postgres.CreateUser(wantUser)
+	s.Equal(nil, err)
+
+	s.expectSelectQueryToReturnOneRow(
+		"user_account",
+		map[string]string{"id": wantUser.Id},
+	)
+}
+
+func (s *IntegrationTestSuite) TestCreateUserViolatesUniqueConstraint() {
+	tests := []userTestCase{
+		{
+			"Should violate unique constraint because ID already exists",
+			routes.User{
+				Id:     s.defaultUser.Id,
+				Email:  "test@gmail.com",
+				Tenant: s.defaultUser.Tenant,
+			},
+		},
+		{
+			"Should violate unique constraint because Email-Tenant combination already exists",
+			routes.User{
+				Id:     "d6cfdb0d-b9ae-4f17-ba0a-b141391c42af",
+				Email:  s.defaultUser.Email,
+				Tenant: s.defaultUser.Tenant,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.name, func() {
+			err := s.postgres.CreateUser(test.input)
+			s.Equal(true, err != nil, "Error should be returned")
+
+			httpErr, ok := err.(*routes.HttpError)
+			s.Equal(true, ok, "Error should be httpError")
+
+			if ok {
+				s.Equal("UNIQUE-VIOLATION-ERROR", httpErr.Code)
+			}
+
+			s.expectSelectQueryToReturnNoRows(
+				"user_account",
+				map[string]string{
+					"id":     test.input.Id,
+					"tenant": test.input.Tenant,
+					"email":  test.input.Email,
+				},
+			)
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestCreateUserDoesNotViolateUniqueConstraint() {
+	// Seed another tenant called "Macdonalds"
+	_, err := s.dbRootConn.Exec("INSERT INTO tenant (name) VALUES ($1)", "Macdonalds")
+	if err != nil {
+		log.Fatalf("Could not seed Macdonalds: %s", err)
+	}
+
+	tests := []userTestCase{
+		{
+			"Should not violate unique constraint because tenant is different",
+			routes.User{
+				Id:     "d6cfdb0d-b9ae-4f17-ba0a-b141391c42af",
+				Email:  s.defaultUser.Email,
+				Tenant: "Macdonalds",
+			},
+		},
+		{
+			"Should not violate unique constraint because email is different",
+			routes.User{
+				Id:     "d6cfdb0d-b9ae-4f17-ba0a-b141391c42af",
+				Email:  "test@gmail.com",
+				Tenant: s.defaultUser.Tenant,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.name, func() {
+			err := s.postgres.CreateUser(test.input)
+			s.Equal(nil, err)
+
+			s.expectSelectQueryToReturnOneRow(
+				"user_account",
+				map[string]string{
+					"id":     test.input.Id,
+					"tenant": test.input.Tenant,
+					"email":  test.input.Email,
+				},
+			)
+
+			query := "DELETE FROM user_account WHERE id = $1"
+			s.dbRootConn.Exec(query, test.input.Id)
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestCreateUserViolatesForeignKeyConstraint() {
+	tests := []userTestCase{
+		{
+			"Should violate foreign key constraint because tenant does not exist",
+			routes.User{
+				Id:     "d6cfdb0d-b9ae-4f17-ba0a-b141391c42af",
+				Email:  s.defaultUser.Email,
+				Tenant: "Macdonalds",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.name, func() {
+			err := s.postgres.CreateUser(test.input)
+			s.Equal(true, err != nil, "Error should be returned")
+
+			httpError, ok := err.(*routes.HttpError)
+			s.Equal(true, ok, "Error should be httpError")
+
+			if ok {
+				s.Equal("INVALID-FOREIGN-KEY-ERROR", httpError.Code)
+			}
+
+			s.expectSelectQueryToReturnNoRows(
+				"user_account",
+				map[string]string{
+					"id":     test.input.Id,
+					"tenant": test.input.Tenant,
+					"email":  test.input.Email,
+				},
+			)
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestGetUsers() {
+	// Seed new users
+	_, err := s.dbRootConn.Exec("INSERT INTO tenant (name) VALUES ($1)", "Macdonalds")
+	if err != nil {
+		log.Fatalf("Could not seed tenant: %s", err)
+	}
+
+	newUsers := []routes.User{
+		{
+			Tenant: "HRIS Enterprises",
+			Email:  "test1@gmail.com",
+		},
+		{
+			Tenant: "Macdonalds",
+			Email:  "test1@gmail.com",
+		},
+		{
+			Tenant: "Macdonalds",
+			Email:  "test2@gmail.com",
+		},
+	}
+
+	for _, user := range newUsers {
+		query := `INSERT INTO user_account (tenant, email, password, totp_secret_key) VALUES ($1, $2, $3, $4)`
+		_, err = s.dbRootConn.Exec(query, user.Tenant, user.Email, "", "")
+		if err != nil {
+			log.Fatalf("Could not seed user: %s", err)
+		}
+	}
+
+	tests := []struct {
+		name  string
+		input routes.User
+		want  int
+	}{
+		{
+			"Should return users by tenant",
+			routes.User{
+				Tenant: "Macdonalds",
+			},
+			2,
+		},
+		{
+			"Should return users by email",
+			routes.User{
+				Email: "test1@gmail.com",
+			},
+			2,
+		},
+		{
+			"Should return users by tenant & email",
+			routes.User{
+				Tenant: "Macdonalds",
+				Email:  "test1@gmail.com",
+			},
+			1,
+		},
+		{
+			"Should return all users",
+			routes.User{},
+			4,
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.name, func() {
+			users, err := s.postgres.GetUsers(test.input)
+			s.Equal(nil, err)
+
+			s.Equal(test.want, len(users), fmt.Sprintf("Should have returned %v row(s)", test.want))
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestCreateAppointment() {
+	wantAppointment := routes.Appointment{
+		Title:      "Manager",
+		Tenant:     s.defaultAppointment.Tenant,
+		Division:   s.defaultAppointment.Division,
+		Department: s.defaultAppointment.Department,
+		UserId:     s.defaultAppointment.UserId,
+		StartDate:  s.defaultAppointment.StartDate,
+	}
+
+	err := s.postgres.CreateAppointment(wantAppointment)
+	s.Equal(nil, err)
+
+	s.expectSelectQueryToReturnOneRow(
+		"appointment",
+		map[string]string{
+			"title": "Manager",
+			"tenant":          s.defaultAppointment.Tenant,
+			"division":        s.defaultAppointment.Division,
+			"department":      s.defaultAppointment.Department,
+			"user_account_id": s.defaultAppointment.UserId,
+			"start_date":      s.defaultAppointment.StartDate,
+		},
+	)
+}
+
+func (s *IntegrationTestSuite) TestCreateAppointmentViolatesUniqueConstraint() {
+	err := s.postgres.CreateAppointment(s.defaultAppointment)
+	s.expectErrorCode(err, "UNIQUE-VIOLATION-ERROR")
+
+	s.expectSelectQueryToReturnOneRow(
+		"appointment",
+		map[string]string{
+			"title": s.defaultAppointment.Title,
+			"tenant":          s.defaultAppointment.Tenant,
+			"division":        s.defaultAppointment.Division,
+			"department":      s.defaultAppointment.Department,
+			"user_account_id": s.defaultAppointment.UserId,
+			"start_date":      s.defaultAppointment.StartDate,
+		},		
+	)
+}
+
+func (s *IntegrationTestSuite) TestCreateAppointmentDoesNotViolateUniqueConstraint() {
+	// Seed another user and department
+	query := "INSERT INTO user_account (id, tenant, email, password, totp_secret_key) VALUES ($1, $2, $3, $4, $5)"
+	_, err := s.dbRootConn.Exec(query, "bc1a0220-fdef-403c-ae0b-5664daefb328", s.defaultUser.Tenant, "test1@gmail.com", "", "")
+	if err != nil {
+		log.Fatalf("Could not seed user: %s", err)
+	}
+
+	query = "INSERT INTO department (tenant, division, name) VALUES ($1, $2, $3)"
+	_, err = s.dbRootConn.Exec(query, s.defaultAppointment.Tenant, s.defaultAppointment.Division, "Customer Support")
+	if err != nil {
+		log.Fatalf("Could not seed user: %s", err)
+	}
+
+	tests := []struct{
+		name string
+		input routes.Appointment
+	} {
+		{
+			"Should not violate unique constraint as the title is different",
+			routes.Appointment{
+				Title: "Manager",
+				Tenant: s.defaultAppointment.Tenant,
+				Division: s.defaultAppointment.Division,
+				Department: s.defaultAppointment.Department,
+				UserId:  s.defaultAppointment.UserId,
+				StartDate: "2024-02-01",
+			},			
+		},				
+		{
+			"Should not violate unique constraint as the user id is different",
+			routes.Appointment{
+				Title: s.defaultAppointment.Title,
+				Tenant: s.defaultAppointment.Tenant,
+				Division: s.defaultAppointment.Division,
+				Department: s.defaultAppointment.Department,
+				UserId: "bc1a0220-fdef-403c-ae0b-5664daefb328",
+				StartDate: "2024-02-01",				
+			},
+		},
+		{
+			"Should not violate unique constraint as the department is different",
+			routes.Appointment{
+				Title: s.defaultAppointment.Title,
+				Tenant: s.defaultAppointment.Tenant,
+				Division: s.defaultAppointment.Division,
+				Department: "Customer Support",
+				UserId: s.defaultUser.Id,
+				StartDate: "2024-02-01",				
+			},
+		},		
+	}
+
+	for _, test := range tests {
+		s.Run(test.name, func() {
+			err := s.postgres.CreateAppointment(test.input)
+			s.Equal(nil, err)
+
+			s.expectSelectQueryToReturnOneRow(
+				"appointment",
+				map[string]string{
+					"title":           test.input.Title,
+					"tenant":          test.input.Tenant,
+					"division":        test.input.Division,
+					"department":      test.input.Department,
+					"user_account_id": test.input.UserId,
+					"start_date":      test.input.StartDate,
+				},					
+			)
+
+			query := "DELETE FROM appointment WHERE title = $1 AND tenant = $2 AND division = $3 AND department = $4 AND user_account_id = $5"
+			s.dbRootConn.Exec(query, test.input.Tenant, test.input.Division, test.input.Department, test.input.UserId, test.input.StartDate)
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestCreateAppointmentViolatesForeignKeyConstraint() {
+	tests := []struct{
+		name string
+		input routes.Appointment
+	} {
+		{
+			"Should violate foreign key constraint because department does not exist",
+			routes.Appointment{
+				Title: s.defaultAppointment.Title,
+				Tenant: "Does",
+				Division: "Not",
+				Department: "Exist",
+				UserId: s.defaultUser.Id,
+				StartDate: "2024-02-01",				
+			},
+		},
+		{
+			"Should violate foreign key constraint because user id does not exist",
+			routes.Appointment{
+				Title: s.defaultAppointment.Title,
+				Tenant: s.defaultAppointment.Tenant,
+				Division: s.defaultAppointment.Division,
+				Department: s.defaultAppointment.Division,
+				UserId: "bc1a0220-fdef-433c-ae0b-5664daefb328",
+				StartDate: "2024-02-01",				
+			},			
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.name, func() {
+			err := s.postgres.CreateAppointment(test.input)
+			s.expectErrorCode(err, "INVALID-FOREIGN-KEY-ERROR")
+
+			s.expectSelectQueryToReturnNoRows(
+				"appointment",
+				map[string]string{
+					"title":           test.input.Title,
+					"tenant":          test.input.Tenant,
+					"division":        test.input.Division,
+					"department":      test.input.Department,
+					"user_account_id": test.input.UserId,
+					"start_date":      test.input.StartDate,
+				},						
+			)
+		})
+	}
 }
