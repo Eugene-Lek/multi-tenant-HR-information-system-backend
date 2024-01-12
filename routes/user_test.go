8c210d7e58c23d7ad5db2c6c55bc42fe418ec0a1
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2024-01-12 23:05:32 +0800

    add: create user tests

--- routes/user_test.go
+++ routes/user_test.go
@@ -14,6 +14,131 @@ import (
 	"multi-tenant-HR-information-system-backend/storage"
 )
 
+func (s *IntegrationTestSuite) TestCreateUser() {
+	wantUser := storage.User{
+		Id: "054cf786-7a54-4ebe-9cb7-d9750bbdedac",
+		TenantId: s.defaultTenant.Id,
+		Email: "test@gmail.com",
+	}
+
+	type requestBody struct {
+		Email string
+	}
+	reqBody := requestBody{
+		Email: wantUser.Email,
+	}
+
+	bodyBuf := new(bytes.Buffer)
+	json.NewEncoder(bodyBuf).Encode(reqBody)
+
+	r, err := http.NewRequest("POST", fmt.Sprintf("/api/tenants/%s/users/%s", wantUser.TenantId, wantUser.Id), bodyBuf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.addSessionCookieToRequest(r, s.defaultUser.Id, s.defaultUser.TenantId, s.defaultUser.Email)
+
+	w := httptest.NewRecorder()
+	s.router.ServeHTTP(w, r)
+
+	s.expectHttpStatus(w, 201)
+	
+	s.expectSelectQueryToReturnOneRow(
+		"user_account",
+		map[string]string {
+			"id": wantUser.Id,
+			"tenant_id": wantUser.TenantId,
+			"email": wantUser.Email,
+		},
+	)
+
+	reader := bufio.NewReader(s.logOutput)
+	s.expectNextLogToContain(reader, `"level":"INFO"`, `"msg":"USER-AUTHORISED"`)
+	s.expectNextLogToContain(reader, `"level":"INFO"`, `"msg":"USER-CREATED"`)	
+	s.expectNextLogToContain(reader, `"level":"INFO"`, `"msg":"REQUEST-COMPLETED"`)	
+}
+
+func (s *IntegrationTestSuite) TestCreateUserInvalidUser() {
+	wantUser := storage.User{
+		Id: "054cf786-7a54-4ebe-9cb7-d9750bbdedac",
+		TenantId: s.defaultTenant.Id,
+		Email: "    ",
+	}
+
+	type requestBody struct {
+		Email string
+	}
+	reqBody := requestBody{
+		Email: wantUser.Email,
+	}
+
+	bodyBuf := new(bytes.Buffer)
+	json.NewEncoder(bodyBuf).Encode(reqBody)
+
+	r, err := http.NewRequest("POST", fmt.Sprintf("/api/tenants/%s/users/%s", wantUser.TenantId, wantUser.Id), bodyBuf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.addSessionCookieToRequest(r, s.defaultUser.Id, s.defaultUser.TenantId, s.defaultUser.Email)
+
+	w := httptest.NewRecorder()
+	s.router.ServeHTTP(w, r)
+
+	s.expectHttpStatus(w, 400)
+	s.expectErrorCode(w, "INPUT-VALIDATION-ERROR")
+	
+	s.expectSelectQueryToReturnNoRows(
+		"user_account",
+		map[string]string {
+			"id": wantUser.Id,
+			"tenant_id": wantUser.TenantId,
+			"email": wantUser.Email,
+		},
+	)
+
+	reader := bufio.NewReader(s.logOutput)
+	s.expectNextLogToContain(reader, `"level":"INFO"`, `"msg":"USER-AUTHORISED"`)
+	s.expectNextLogToContain(reader, `"level":"WARN"`, `"msg":"INPUT-VALIDATION-ERROR"`)	
+	s.expectNextLogToContain(reader, `"level":"INFO"`, `"msg":"REQUEST-COMPLETED"`)	
+}
+
+func (s *IntegrationTestSuite) TestCreateUserViolatesUniqueConstraint() {
+	type requestBody struct {
+		Email string
+	}
+	reqBody := requestBody{
+		Email: s.defaultUser.Email,
+	}
+
+	bodyBuf := new(bytes.Buffer)
+	json.NewEncoder(bodyBuf).Encode(reqBody)
+
+	r, err := http.NewRequest("POST", fmt.Sprintf("/api/tenants/%s/users/%s", s.defaultUser.TenantId, s.defaultUser.Id), bodyBuf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.addSessionCookieToRequest(r, s.defaultUser.Id, s.defaultUser.TenantId, s.defaultUser.Email)
+
+	w := httptest.NewRecorder()
+	s.router.ServeHTTP(w, r)
+
+	s.expectHttpStatus(w, 409)
+	s.expectErrorCode(w, "UNIQUE-VIOLATION-ERROR")
+	
+	s.expectSelectQueryToReturnOneRow(
+		"user_account",
+		map[string]string {
+			"id": s.defaultUser.Id,
+			"tenant_id": s.defaultUser.TenantId,
+			"email": s.defaultUser.Email,
+		},
+	)
+
+	reader := bufio.NewReader(s.logOutput)
+	s.expectNextLogToContain(reader, `"level":"INFO"`, `"msg":"USER-AUTHORISED"`)
+	s.expectNextLogToContain(reader, `"level":"WARN"`, `"msg":"UNIQUE-VIOLATION-ERROR"`)	
+	s.expectNextLogToContain(reader, `"level":"INFO"`, `"msg":"REQUEST-COMPLETED"`)	
+}
+
 func (s *IntegrationTestSuite) TestCreateAppointment() {
 	wantAppointment := storage.Appointment{
 		Id: "cfc61cce-3d5a-4014-8490-3302ddd187b8",
